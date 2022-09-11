979bcd823d891e9b8c09b50f86ff5db3e394e66d
emdad <emdad@ergo-ventures.com>
2022-09-11 16:37:14 +0600

    arrays_with_pointers.c

--- chapter07-arrays/07_06_arrays_with_pointers.c
+++ chapter07-arrays/07_06_arrays_with_pointers.c
@@ -4,7 +4,8 @@ int main()
 {
     int marks[4];
     int *ptr;
-    ptr = &marks[0];
+    // ptr = &marks[0];
+    ptr=marks; // can also be initialized using pointer and arrays.
     for (int i = 0; i < 4; i++)
     {
         printf("enter the value of marks for student %d: \n", i + 1);
