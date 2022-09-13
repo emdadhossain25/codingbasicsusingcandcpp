4e33811f062a8d2370afc5b7a29443216951bd4d
emdad <emdad@ergo-ventures.com>
2022-09-13 21:39:42 +0600

    strings iteration with \0 termination checked

--- chapter08-strings/08_02_print_strings.c
+++ chapter08-strings/08_02_print_strings.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-    char str[] = "EMDAD";
+    // char str[] = "EMDAD";
+    char str[] = {'E','M','D','A','D','\0'};
     char *ptr = str;
     // pointer increment done using pointer arithmatic and checks the last char for string in c
     // which is '\0' null character
