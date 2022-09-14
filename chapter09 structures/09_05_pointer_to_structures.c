853dc9176b1e372229096c27bd2820fcb08f87c4
emdad <emdad@ergo-ventures.com>
2022-09-14 20:24:21 +0600

    pointer with arrow operator

--- chapter09 structures/09_05_pointer_to_structures.c
+++ chapter09 structures/09_05_pointer_to_structures.c
@@ -8,18 +8,18 @@ struct employee
     char name[10];
 };
 
-
 int main()
 {
     struct employee e1;
     struct employee *ptr;
-    ptr=&e1;
-    (*ptr).code=101;
-    strcpy(e1.name,"emdad");
+    ptr = &e1;
+    // (*ptr).code=101; --> or you can write
+    ptr->code = 101;
+    strcpy(e1.name, "emdad");
     (*ptr).salary = 101.23;
-    printf("code is %d\n",e1.code);
-    printf("name is %s\n",(*ptr).name);
-    printf("salary is %.2f\n",(*ptr).salary);
-    printf("salary is %.2f\n",e1.salary);
+    printf("code is %d\n", e1.code);
+    printf("name is %s\n", (*ptr).name);
+    printf("salary is %.2f\n", (*ptr).salary);
+    printf("salary is %.2f\n", e1.salary);
     return 0;
 }
