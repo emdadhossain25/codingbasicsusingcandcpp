c519360fb5dcc1ed8b476493407fa0ebc87df3dd
emdad <emdad@ergo-ventures.com>
2022-09-15 18:57:59 +0600

    10 02 reading from file using fscanf()

--- chapter10_file_i/10_02_file_reading.c
+++ chapter10_file_i/10_02_file_reading.c
@@ -2,9 +2,12 @@
 
 int main(){
     FILE *ptr;
-    int num;
+    int num,num2;
     ptr = fopen("Emdad.txt","r");
     fscanf(ptr,"%d",&num);
+    fscanf(ptr,"%d",&num2);
+    fclose(ptr);
     printf("the value of num is %d\n",num);
+    printf("the value of num2 is %d\n",num2);
     return 0;
 }
