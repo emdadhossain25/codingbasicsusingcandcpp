58f35e79dcd5b322cb544fd86248ffc1b382a491
emdad <emdad@ergo-ventures.com>
2022-09-15 21:08:39 +0600

    read from a file and write twice into a file using read and write operation

---
+++ chapter10_file_i/10_09_pr03.c
@@ -0,0 +1,31 @@
+// write a program to read a character from a file and write twice its content into a file;
+
+#include <stdio.h>
+
+int main()
+{
+    FILE *ptr, *ptw;
+    char c;
+    ptw = fopen("writeintotwice.txt", "w");
+    int count =0;
+    while (count < 2)
+    {
+
+        ptr = fopen("readfrom.txt", "r");
+        c = fgetc(ptr);
+
+        while (c != EOF)
+        {
+
+            putc(c, ptw);
+            c = fgetc(ptr);
+        }
+        putc('\n', ptw);
+
+        fclose(ptr);
+        count++;
+    }
+
+    fclose(ptw);
+    return 0;
+}
