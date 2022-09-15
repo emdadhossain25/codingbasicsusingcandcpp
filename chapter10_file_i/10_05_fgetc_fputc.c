de71d148ec53d6a115aea5c650ed377bcb86829a
emdad <emdad@ergo-ventures.com>
2022-09-15 19:34:59 +0600

    fixed putc and getc

--- chapter10_file_i/10_05_fgetc_fputc.c
+++ chapter10_file_i/10_05_fgetc_fputc.c
@@ -2,16 +2,19 @@
 
 int main()
 {
-    FILE *ptr;
+    FILE *ptr, *ptrc;
     ptr = fopen("getcdemo.txt", "r");
-    // char c = fgetc(ptr);
-    printf("the value of my character is %c\n",fgetc(ptr));
-    printf("the value of my character is %c\n",fgetc(ptr));
-    printf("the value of my character is %c\n",fgetc(ptr));
-    printf("the value of my character is %c\n",fgetc(ptr));
-    printf("the value of my character is %c\n",fgetc(ptr));
-    printf("the value of my character is %c\n",fgetc(ptr));
+    char c = fgetc(ptr);
+    printf("the value of my character is %c\n", fgetc(ptr));
+    printf("the value of my character is %c\n", fgetc(ptr));
+    printf("the value of my character is %c\n", fgetc(ptr));
+    printf("the value of my character is %c\n", fgetc(ptr));
+    printf("the value of my character is %c\n", fgetc(ptr));
+    printf("the value of my character is %c\n", fgetc(ptr));
     fclose(ptr);
+    ptrc = fopen("putcdemo.txt", "w");
+    putc('v', ptrc);
+    fclose(ptrc);
 
     return 0;
 }
